93fd08b402b5e26d09ba38fd4b6f361de45219ef
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2018-06-02 16:27:16 -0700

    Add sample course and display it

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -12,6 +12,12 @@ import { CoursesService } from './courses.service';
               <img [src]="imageUrl" />
               <br>
               <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+              <br><br>
+              {{ course.title }} <br>
+              {{ course.students }} <br>
+              {{ course.rating | number }} <br>
+              {{ course.price }} <br>
+              {{ course.releaseDate }}
             `
 })
 export class CoursesComponent {
@@ -19,7 +25,13 @@ export class CoursesComponent {
   courses;
   email = "me@example.com";
   imageUrl = "https://picsum.photos/400/200";
-
+  course = {
+    title: "The Complete Angular Course",
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  }
 
 
   constructor(service: CoursesService) {
