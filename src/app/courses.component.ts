d29090ec79f125f032795600ffac42ea7c8da3aa
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2018-06-02 16:54:22 -0700

    Add description field to course object

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -14,6 +14,7 @@ import { CoursesService } from './courses.service';
               <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
               <br><br>
               {{ course.title }} <br>
+              {{ course.description }} <br>
               {{ course.students | number }} <br>
               {{ course.rating | number:'1.2-2' }} <br>
               {{ course.price | currency }} <br>
@@ -27,6 +28,7 @@ export class CoursesComponent {
   imageUrl = "https://picsum.photos/400/200";
   course = {
     title: "The Complete Angular Course",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet diam dolor, vitae malesuada est tristique facilisis. Vivamus viverra sodales pulvinar. Vestibulum sit amet sagittis odio. Curabitur id arcu porta, sollicitudin metus vel, tincidunt risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur dictum vulputate lacinia. Pellentesque semper quam urna, in mattis nisi aliquam et. In lacinia felis non enim feugiat, consectetur laoreet leo dapibus. Praesent in gravida massa. Morbi dignissim magna quis pulvinar pulvinar. Aenean aliquam et eros nec scelerisque. Aenean id turpis sem. Donec in pellentesque nibh. Mauris nisi nulla, condimentum quis egestas non, accumsan sit amet elit. Nam dictum augue ut turpis consectetur accumsan. Sed ante magna, blandit ac fringilla sit amet, interdum a nunc. Nam vel lobortis risus, quis commodo ligula. Donec tincidunt sit amet velit ac ornare. Donec aliquet at arcu fermentum rhoncus. Pellentesque eu consectetur lorem. Suspendisse potenti. In vestibulum enim sem. Sed id dictum quam. Suspendisse malesuada augue vel lorem egestas sollicitudin et ut velit. Phasellus luctus facilisis ultrices.",
     rating: 4.9745,
     students: 30123,
     price: 190.95,
